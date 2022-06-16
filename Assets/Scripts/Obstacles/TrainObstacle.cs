6445ede088123ae02cf85b631e959c0af71d2c41
harshdulani <harshdulani@yahoo.in>
2022-06-16 18:35:15 +0530

    made prefab of coaster obstacle in previous commit, added prefab now

---
+++ Assets/Scripts/Obstacles/TrainObstacle.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class TrainObstacle : MonoBehaviour
+{
+	[SerializeField] private BridgeLight bridgeLight;
+	[SerializeField] private Transform train;
+	[SerializeField] private float travelLoopDistance, travelLoopDuration;
+
+	private void Start()
+	{
+		train.position -= train.forward * travelLoopDistance / 2;
+		train.DOMove(train.position + train.forward * travelLoopDistance, travelLoopDuration);
+	}
+}
