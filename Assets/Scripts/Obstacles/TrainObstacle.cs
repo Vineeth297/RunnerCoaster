1db7e612d85e45384b8157d4ea67f7ba92421ec4
harshdulani <harshdulani@yahoo.in>
2022-06-17 19:48:08 +0530

    added script for train obstacle but not meshes

--- Assets/Scripts/Obstacles/TrainObstacle.cs
+++ Assets/Scripts/Obstacles/TrainObstacle.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 
 public class TrainObstacle : MonoBehaviour
 {
-	[SerializeField] private BridgeLight bridgeLight;
 	[SerializeField] private Transform train;
 	[SerializeField] private float travelLoopDistance, travelLoopDuration;
 
 	private void Start()
 	{
-		train.position -= train.forward * travelLoopDistance / 2;
-		train.DOMove(train.position + train.forward * travelLoopDistance, travelLoopDuration);
+		train.DOLocalMove(train.localPosition + train.forward * travelLoopDistance, travelLoopDuration);
 	}
 }
--- Assets/Scripts/PickupPlatform.cs
+++ Assets/Scripts/PickupPlatform.cs
@@ -11,27 +11,34 @@ public class PickupPlatform : MonoBehaviour
 	{
 		var temp = 0f;
 		
-		DOTween.To(() => temp, value => temp = value, 1f, 0.5f).SetEase(Ease.InOutCubic)
+		DOTween.To(() => temp, value => temp = value, 1f, 0.5f)
+			.SetEase(Ease.InOutCubic)
 			.OnUpdate(() =>
 			{
 				passengers[0].transform.position = Vector3.Lerp(passengers[0].position, kartPassenger1.position, temp);
 				passengers[1].transform.position = Vector3.Lerp(passengers[1].position, kartPassenger2.position, temp);
 			});
 
-		passengers[0].DOMoveY(passengers[0].position.y + jumpHeight, 0.25f).SetEase(Ease.OutExpo)
-			.OnComplete(() => passengers[0].DOMoveY(kartPassenger1.position.y, 0.25f).SetEase(Ease.InExpo)
-				.OnComplete(() =>
-				{
-					kartPassenger1.gameObject.SetActive(true);
-					passengers[0].gameObject.SetActive(false);
-				}));
+		passengers[0].DOMoveY(passengers[0].position.y + jumpHeight, 0.25f)
+			.SetEase(Ease.OutExpo)
+			.OnComplete(() => 
+				passengers[0].DOMoveY(kartPassenger1.position.y, 0.25f)
+					.SetEase(Ease.InExpo)
+					.OnComplete(() =>
+					{
+						kartPassenger1.gameObject.SetActive(true);
+						passengers[0].gameObject.SetActive(false);
+					}));
 		
-		passengers[1].DOMoveY(passengers[0].position.y + jumpHeight, 0.25f).SetEase(Ease.OutExpo)
-			.OnComplete(() => passengers[0].DOMoveY(kartPassenger2.position.y, 0.25f).SetEase(Ease.InExpo)
-				.OnComplete(() =>
-				{
-					kartPassenger2.gameObject.SetActive(true);
-					passengers[1].gameObject.SetActive(false);
-				}));
+		passengers[1].DOMoveY(passengers[0].position.y + jumpHeight, 0.25f)
+			.SetEase(Ease.OutExpo)
+			.OnComplete(() => 
+				passengers[0].DOMoveY(kartPassenger2.position.y, 0.25f)
+					.SetEase(Ease.InExpo)
+					.OnComplete(() =>
+					{
+						kartPassenger2.gameObject.SetActive(true);
+						passengers[1].gameObject.SetActive(false);
+					}));
 	}
 }
