a11c927a02b1f9f05366f56c62e8c516203a0001
Vineeth <mamidipallyvineeth@gmail.com>
2022-08-01 14:20:47 +0530

    Commit on 01.08.22 Adding idle dinos

--- Assets/ScatterTheBirds.cs
+++ Assets/ScatterTheBirds.cs
@@ -8,10 +8,13 @@ public class ScatterTheBirds : MonoBehaviour
     // Start is called before the first frame update
 	[SerializeField] private List<GameObject> birds;
 	[SerializeField] private float maxFlySpeed = 1f;
-	
+	[SerializeField] private float forwardMultiplier = 330f;
 	[SerializeField] private List<Animator> _birdAnimators;
 
+	[SerializeField] private bool allToFlyAtSameTime;
 	private static readonly int FlyHash = Animator.StringToHash("Fly");
+	
+	
 	
 	private void Start()
     {
@@ -35,36 +38,63 @@ public class ScatterTheBirds : MonoBehaviour
 	{
 		if (!other.CompareTag("Player") && !other.CompareTag("Kart")) return;
 
-		StartCoroutine(ScatterTheBirdsAway());
+		if(!allToFlyAtSameTime)
+			StartCoroutine(ScatterTheBirdsAway());
+		else
+		{
+			ScatterTheBirdsAways();
+		}
 	}
 
 	private IEnumerator ScatterTheBirdsAway()
-	{/*
-		for (var bird = 0; bird < birds.Count; bird++)
-		{
-			var x = birds[bird].transform.forward + birds[bird].transform.up;
-			birds[bird].transform.DOMove(birds[bird].transform.position + (x * 15f), 1).SetEase(Ease.InCirc);
-			_birdAnimators[bird].SetTrigger(FlyHash);
-			//_birdAnimators[bird].speed = Random.Range(1, 3);
-			yield return new WaitForSeconds(0.1f);
-		}*/
+	{
 		AudioManager.instance.Play("BirdsFlap");
 		
 		for (var bird = 0; bird < birds.Count; bird++)
 		{
-			// Vector3.Normalize(transform.forward + transform.right * (.5f * (Random.value > 0.5f ? 1 : -1))) * 50
-			// 	+ Vector3.up * 10 ;
 			var myBird = birds[bird].transform;
-			var pos = transform.localPosition +
-					  transform.forward + transform.right * (.5f * (Random.value > 0.5f ? 1 : -1) * 50)
-				+ Vector3.up * 10 ;
+
+			var direction = transform.forward * forwardMultiplier + transform.right * ((Random.value > 0.5f ? 1 : -1) * 50)
+											  + Vector3.up * 30;
 			
-			myBird.DOLocalMoveX(pos.x, 1.5f).SetEase(Ease.Linear).OnComplete(()=>myBird.gameObject.SetActive(false));
-			myBird.DOLocalMoveY(pos.y, 1.5f).SetEase(Ease.Linear);
-			myBird.DOLocalMoveZ(pos.z + Random.Range(100,200), 1.5f).SetEase(Ease.Linear);
+			Debug.DrawRay(myBird.position, direction, Color.red, 3f, false);
+			
+			var endPos = myBird.position + direction;
+					  
+			
+			myBird.DOMoveX(endPos.x, 1.5f).SetEase(Ease.Linear).OnComplete(()=>myBird.gameObject.SetActive(false));
+			myBird.DOMoveY(endPos.y, 1.5f).SetEase(Ease.Linear);
+			myBird.DOMoveZ(endPos.z, 1.5f).SetEase(Ease.Linear);
 
 			_birdAnimators[bird].SetTrigger(FlyHash);
-			yield return new WaitForSeconds(0.1f);
+			yield return new WaitForSeconds(0.15f);
+
 		}
+
+	}
+	
+	private void ScatterTheBirdsAways()
+	{
+		AudioManager.instance.Play("BirdsFlap");
+		
+		for (var bird = 0; bird < birds.Count; bird++)
+		{
+			var myBird = birds[bird].transform;
+
+			var direction = transform.forward * forwardMultiplier + transform.right * ((Random.value > 0.5f ? 1 : -1) * 50)
+																  + Vector3.up * 30;
+			
+			Debug.DrawRay(myBird.position, direction, Color.red, 3f, false);
+			
+			var endPos = myBird.position + direction;
+					  
+			
+			myBird.DOMoveX(endPos.x, 1.5f).SetEase(Ease.Linear).OnComplete(()=>myBird.gameObject.SetActive(false));
+			myBird.DOMoveY(endPos.y, 1.5f).SetEase(Ease.Linear);
+			myBird.DOMoveZ(endPos.z, 1.5f).SetEase(Ease.Linear);
+
+			_birdAnimators[bird].SetTrigger(FlyHash);
+		}
+
 	}
 }
